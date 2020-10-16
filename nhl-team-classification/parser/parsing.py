5e13a65a25a48b65283124c18ade9d16a0151b03
Alexey Topolnitskiy <alextopol2903@gmail.com>
2020-10-16 16:19:20 +0300

    Update parser and add data

--- nhl-team-classification/parser/parsing.py
+++ nhl-team-classification/parser/parsing.py
@@ -1,2 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from tqdm import tqdm
+import csv
+
+save_path = "/Users/alexeytopolnitskiy/Projects/dl-for-hockey/nhl-team-classification/parser/players_photos.csv"
+
+df = pd.read_csv("/Users/alexeytopolnitskiy/Projects/dl-for-hockey/nhl-players-data/players-data/players_info.csv", sep=";")
+players_sites = df.loc[:, "Webpage"]
+players_photo_urls = []
+
+for i in tqdm(range(len(players_sites))):
+
+    url = players_sites[i]
+    html = requests.get(url).content
+    soup = BeautifulSoup(html, "html.parser")
+
+    player_photo_1 = soup.find("div", class_="player-jumbotron player-jumbotron--responsive")
+    player_photo_2 = player_photo_1.find("div", class_="player-jumbotron-cover__image")
+    player_photo = player_photo_2.get("data-img")
+    players_photo_urls.append(player_photo)
+
+with open(save_path, "w", newline="") as f:
+    writer = csv.writer(f, delimiter=";")
+    writer.writerow([
+        "Team",
+        "Photo"
+    ])
+    for j in tqdm(range(len(players_photo_urls))):
+        writer.writerow([
+            df.loc[j, "Team"],
+            players_photo_urls[j]
+        ])
